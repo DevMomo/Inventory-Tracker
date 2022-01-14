e130b7914d77046ffbf4a2036654e9bbbab977bb
Monir Imamverdi <monir.imamverdi@gmail.com>
2022-01-14 01:14:58 -0700

    Created get products end point

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,29 @@ function App() {
     console.log(name, category, description);
   };
 
+  const [productsList, setProductsList] = useState([]);
+
   const addProduct = () => {
-    Axios.post("http://localhost:3000/create", {
+    Axios.post("http://localhost:3001/create", {
       name: name,
       category: category,
       description: description,
     }).then(() => {
-      console.log("addProduct call success");
+      console.log(name, category, description);
+      setProductsList([
+        ...productsList,
+        {
+          product_name: name,
+          category_id: category,
+          product_description: description,
+        },
+      ]);
+    });
+  };
+
+  const getProducts = () => {
+    Axios.get("http://localhost:3001/products", {}).then((response) => {
+      setProductsList(response.data);
     });
   };
 
@@ -46,6 +62,23 @@ function App() {
           }}
         />
         <button onClick={addProduct}>Add</button>
+      </div>
+      <hr />
+      <div className="products">
+        <button onClick={getProducts}>Show Products</button>
+
+        {productsList.map((val, key) => {
+          return (
+            <div className="product">
+              <h4>Name: {val.product_name}</h4>
+              <h4>Category: {val.category_id}</h4>
+              <h4>
+                Description:{" "}
+                <p className="productDescription">{val.product_description}</p>
+              </h4>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const e = require("express");
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
   user: "root",
@@ -16,8 +20,8 @@ app.post("/create", (req, res) => {
   const description = req.body.description;
 
   db.query(
-    "INSERT INTO products (name, category, description) VALUES (?,?,?)",
-    [product_name, category_id, product_description],
+    "INSERT INTO products (product_name, category_id, product_description) VALUES (?,?,?)",
+    [name, category, description],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -26,6 +30,16 @@ app.post("/create", (req, res) => {
       }
     }
   );
+});
+
+app.get("/products", (req, res) => {
+  db.query("SELECT * FROM products", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.listen(3001, () => {
