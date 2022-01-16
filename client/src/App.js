fa89cf5bb6bdc64538b3f13055eab8962b5eb050
Monir Imamverdi <monir.imamverdi@gmail.com>
2022-01-16 05:13:33 -0700

    cleaned up project

--- client/src/App.js
+++ client/src/App.js
@@ -134,7 +134,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="Monir's Very Simple Inventory Tracker (Made for Shopify)">
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
