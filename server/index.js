38d6e3761b8731cecd00f2d9c2d1d17aec352876
Monir Imamverdi <monir.imamverdi@gmail.com>
2022-01-16 04:51:45 -0700

    Added backend documentation

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const e = require("express");
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
@@ -16,6 +15,13 @@ const db = mysql.createConnection({
   database: "inventorysystem",
 });
 
+/*
+ * /create:
+ *   POST:
+ *     summary: Adds a new product entry to the products database.
+ *     description: Adds a new product entry to the products database with user specified
+ *     fields "name", "category_id", and "product_description".
+ */
 app.post("/create", (req, res) => {
   const name = req.body.name;
   const category = req.body.category;
@@ -34,6 +40,11 @@ app.post("/create", (req, res) => {
   );
 });
 
+/*
+ * /products:
+ *   GET:
+ *     summary: Retrieve all product entries from the products database.
+ */
 app.get("/products", (req, res) => {
   db.query("SELECT * FROM products", (err, result) => {
     if (err) {
@@ -44,29 +55,36 @@ app.get("/products", (req, res) => {
   });
 });
 
+/*
+ * /products/download:
+ *   GET:
+ *     summary: Download all product entries from the products database in CSV format.
+ *     description: 
+ *       - Retrieve all product entries from the products database
+ *       - Parse DB result into JSON
+ *       - Write JSON data into CSV file
+ *       - Serve CSV file as a download
+ */
 app.get("/products/download", (req, res) => {
   db.query("SELECT * FROM products", (err, result) => {
     if (err) {
-      console.log("failed to create products.csv.");
       console.log(err);
     } else {
       const jsonData = JSON.parse(JSON.stringify(result));
-
       try {
         const json2csvParser = new json2csv();
         var csv = json2csvParser.parse(jsonData);
-        console.log(csv);
+        console.log("CSV parsed.");
       } catch (csvErr) {
         console.log(csvErr);
       }
-
       const fileName = "products" + Date.now() + ".csv";
       const path = "./public/csv/" + fileName;
-
       fs.writeFile(path, csv, function (err, data) {
         if (err) {
           throw err;
         } else {
+          console.log("products.csv created.");
           res.download(path); // This is what you need
         }
       });
@@ -74,6 +92,11 @@ app.get("/products/download", (req, res) => {
   });
 });
 
+/*
+ * /categories:
+ *   GET:
+ *     summary: Retrieve all category entries from categories database.
+ */
 app.get("/categories", (req, res) => {
   db.query("SELECT * FROM categories", (err, result) => {
     if (err) {
@@ -84,6 +107,13 @@ app.get("/categories", (req, res) => {
   });
 });
 
+/*
+ * /update:
+ *   PUT:
+ *     summary: Update a product entry in the products database.
+ *     description: Update a product entry in the products database. 
+ *       If a field is left empty, COALESCE is used to use existing data.
+ */
 app.put("/update", (req, res) => {
   const id = req.body.id;
   const name = req.body.name;
@@ -102,6 +132,11 @@ app.put("/update", (req, res) => {
   );
 });
 
+/*
+ * /delete/:id:
+ *   DELETE:
+ *     summary: Removes an entry from products database given product_id.
+ */
 app.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
   db.query("DELETE FROM products WHERE product_id = ?", id, (err, result) => {
